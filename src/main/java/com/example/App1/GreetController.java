88073e1ef5e6d0e1a867c6b300f7a25e689406c8
jameskm17 <james.km@tech-swing.com>
2026-01-14 18:31:45 +0530

    first commit

---
+++ src/main/java/com/example/App1/GreetController.java
@@ -0,0 +1,27 @@
+package com.example.App1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class GreetController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/greet")
+    public String greet() {
+        // Call App2
+        String app2Response = restTemplate.getForObject("http://app2-service:8082/hello", String.class);
+        return "App1 received: " + app2Response;
+    }
+    
+    @GetMapping("/esakki")
+    public String greetings() {
+        // Call App2
+        String app2Response = restTemplate.getForObject("http://app2-service:8082/Docker", String.class);
+        return "App1 received: " + app2Response;
+    }
+}
